b36bf004373bc041f836392b4bd3236a259739d3
Thang Le <thangle@novahub.vn>
2019-12-18 17:23:09 +0700

    add more tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testPlus() {
+		int sum = 5 + 5;
+		assertThat(10).isEqualTo(sum);
 	}
 
 }
